3e34031524db4940f18d7dc1f1105f2c55ae93db
NeLk <nelk42@gmail.com>
2014-10-24 11:15:35 +0100

    Should return 1 if string is 1

--- src/calc.js
+++ src/calc.js
@@ -1,7 +1,21 @@
 var calc = {
 	add: function (text) {
 		var res = text ? text : 0;
+		if ( res != 0){
+			res = this.getSum(text)
+		}
 		return parseInt(res);
+	},
+	getSum: function (text) {
+		var res = text;
+		if (text.indexOf(',') > 0){
+			var array = res.split(',');
+			res = 0;
+			for (var i = 0; i < array.length; i++) {
+				res += parseInt(array[i])
+			}
+		}
+		return res;
 	}
 }
 
--- test/calc.spec.js
+++ test/calc.spec.js
@@ -10,9 +10,13 @@ describe('The add method of the string calculator', function () {
 	});
 
 	it('should return 1 if string is 1', function () {
-		expect(calc.add(1)).toBe(1);
+		expect(calc.add('1')).toBe(1);
 	});
 
+	it('should return 3 if string is 1,2', function () {
+		expect(calc.add('1,2')).toBe(3);
+	});
 
+	
 
 });
