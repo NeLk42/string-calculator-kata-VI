3765d20c987beb1384018acaf475518a328dbfb9
NeLk <nelk42@gmail.com>
2014-10-24 10:05:44 +0100

    Should return 1 if string is 1

--- test/calc.spec.js
+++ test/calc.spec.js
@@ -5,4 +5,14 @@ describe('The add method of the string calculator', function () {
 		expect(calc.add()).toBe(0);
 	});
 
+	it('should return 0 if string is ', function () {
+		expect(calc.add("")).toBe(0);
+	});
+
+	it('should return 1 if string is 1', function () {
+		expect(calc.add(1)).toBe(1);
+	});
+
+
+
 });
